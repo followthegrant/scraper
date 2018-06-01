c88e60428f7b0caa3ebfbde3e1492401b7fa2a33
Simon Wörpel <simon.woerpel@medienrevolte.de>
2018-06-01 18:23:53 +0200

    Save papers to subfolders of publisher / journal

--- scraper/spiders/paper_spider.py
+++ scraper/spiders/paper_spider.py
@@ -2,6 +2,7 @@ import csv
 import scrapy
 import os
 from datetime import datetime
+from uuid import uuid4
 
 from util import slugify
 
@@ -13,13 +14,19 @@ class PaperSpider(scrapy.Spider):
         with open(self.papers) as f:
             reader = csv.DictReader(f)
             for row in reader:
-                yield scrapy.Request('https://{base_url}{url}'.format(**row), meta=row)  # FIXME http scheme
+                yield scrapy.Request('http://{base_url}{url}'.format(**row), meta=row)  # FIXME http scheme
 
     def parse(self, response):
         self.logger.info('Open: %s' % response.url)
 
         if response.status == 200:
-            fname = os.path.join(self.save_to, '%s.html' % slugify(response.meta['title']))
+            fname = os.path.join(
+                self.save_to,
+                response.meta['publisher_slug'],
+                response.meta['journal_slug'],
+                '%s--%s.html' % (slugify(response.meta['title'][:50]), str(uuid4())[:8])
+            )
+            os.makedirs(os.path.split(fname)[0], exist_ok=True)
             with open(fname, 'w') as f:
                 f.write(response.text)
                 self.logger.info('Saved to: %s' % fname)
